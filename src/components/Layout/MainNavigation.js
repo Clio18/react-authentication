709c3ef49da43b61e9aec76eb6397be6ef6ef06d
U0L9511 <estclio09@gmail.com>
2022-07-05 13:59:21 +0300

    Changing password was added (use ref, context hooks)

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,24 +1,36 @@
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
+
+import AuthContext from "../../store/auth-context";
 
 const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+  const isLogged = authCtx.isLoggedIn;
+
   return (
     <header className={classes.header}>
-      <Link to='/'>
+      <Link to="/">
         <div className={classes.logo}>React Auth</div>
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!isLogged && (
+            <li>
+              <Link to="/auth">Login</Link>
+            </li>
+          )}
+          {isLogged && (
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          )}
+          {isLogged && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
